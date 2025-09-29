aa3389f71034cd8bfa91de6bb5ee2f15aeb2eb93
BustamanteHM <144525250+BustamanteHM@users.noreply.github.com>
2025-09-29 14:15:46 +0800

    Delete ipt final directory

--- ipt final/javascript/contact.js
+++
@@ -1,9 +0,0 @@
- // Clear all form fields and error messages
-    function clearForm() {
-        // Clear form inputs
-        document.getElementById('contactForm').reset();
-
-        // Clear error messages
-        const errorDivs = document.querySelectorAll('.error');
-        errorDivs.forEach(div => div.textContent = '');
-    }s
--- ipt final/test_insert.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'db.php';
-$sql = "INSERT INTO email (name, subject, email, message) VALUES ('Test Name', 'Test Subject', 'test@example.com', 'Test message')";
-if ($conn->query($sql) === TRUE) {
-    echo "Insert successful!";
-} else {
-    echo "Error: " . $conn->error;
-}
-?>
